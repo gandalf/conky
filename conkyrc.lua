8ddab4ccb38fc3de93477c1fdeca0037ba487af8
Anthoni Shogan <anthonir@smartek21.com>
2019-09-06 10:49:16 +0530

    Minor layout and color changes.

--- conkyrc.lua
+++ conkyrc.lua
@@ -11,9 +11,11 @@ conky.config = {
    update_interval = 1,
    alignment = 'top_right',
    gap_x = 50,
+   --gap_y = 50,
    maximum_width = 350,
    minimum_size = [[350, 700]],
    text_buffer_size = 1024,
+   uppercase = true,
 
    -- window
    own_window = true,
@@ -24,45 +26,37 @@ conky.config = {
    own_window_argb_value = 0,
    own_window_argb_visual = true,
    own_window_colour = '#000000',
-
-   -- fonts
-   template0="${font Font Awesome}${font}",
-   template1="${font Font Awesome}${font}"
 }
 
 conky.text = [[
+${color gray}
 ${alignc}${font Ubuntu:pixelsize=40} ${time %H:%M}${font}
+${alignc}${font Ubuntu:pixelsize=15} ${time %A %d %B %Y}${font}${color}
+${font Inconsolata:pixelsize=14}
+${color lightblue}${nodename}:${color} ${sysname} ${kernel}
 
-${alignc}${font Ubuntu:pixelsize=15} ${time %A %d %B %Y}${font}
-${font Inconsolata:pixelsize=15}
+${color orange}system ${hr 2} ${color}
 
-${color yellow}${nodename} ${hr} ${color}
+${color lightblue}uptime:${color} ${uptime} ${alignr}${color lightblue}processes:${color} ${running_processes}/${processes}
 
-$alignc ${sysname} ${kernel}
-
-${color yellow}system ${hr} ${color}
-
-uptime: ${uptime}
-processes: ${running_processes}/${processes}
-
-CPU: $alignr ${cpu}% ${cpubar 10, 106}
-${cpugraph 40,350 7fff00 ff0000 -t}
-RAM: $alignr ${memperc}% ${membar 10, 106}
+${color lightblue}CPU:${color} $alignr ${cpu}% ${color gray}${cpubar 10, 140}${color}
+${cpugraph 40,350 7fff00 ff0000 -t}$
+${color lightblue}RAM:${color} $alignr ${memperc}% ${color gray}${membar 10, 140}${color}
 ${memgraph 40,350 7fff00 ff0000 -t}
-SWAP: $alignr ${swapperc}% ${swapbar 10, 106}
-BATT: $alignr ${battery_percent} % ${battery_bar 10, 106}
+${color lightblue}SWAP:${color} $alignr ${swapperc}% ${color gray}${swapbar 10, 140}${color}
+${color lightblue}BATT:${color} $alignr ${battery_percent}% ${color gray}${battery_bar 10, 140}${color}
 
-FS:
-/: ${alignr} ${fs_used /} / ${fs_size /}
-/home: ${alignr} ${fs_used /home} / ${fs_size /home}
+${color lightblue}/:${color} ${alignr}${fs_used_perc /}% ${alignr}${color gray}${fs_bar 10,140 /}${color}
+${color lightblue}/home:${color} ${alignr}${fs_used_perc /home}% ${alignr}${color gray}${fs_bar 10,140 /home}${color}
 
-${color yellow}network ${addrs enp0s3} ${hr} ${color}
+${color orange}network ${color lightblue}${addrs enp0s3}${color} ${color orange}${hr 2} ${color}
 
-${font Font Awesome}${font} ${font Inconsolata:pixelsize=15}${downspeedf enp0s3} kB/s${font}                  ${font Font Awesome}${font} ${font Inconsolata:pixelsize=15}${upspeedf enp0s3} kB/s${font}
+${font Font Awesome:pixelsize=16}${font} ${font Inconsolata:pixelsize=14}${downspeedf enp0s3} kB/s${font}                  ${font Font Awesome:pixelsize=16}${font} ${font Inconsolata:pixelsize=16}${upspeedf enp0s3} kB/s${font}
 ${downspeedgraph enp0s3 40,170 7fff00 ff0000 -t} ${upspeedgraph enp0s3    40,170 7fff00 ff0000 -t}
-${font Inconsolata:pixelsize=15}
-${color yellow}processes ${hr 1}${color}
-Name $alignr PID   CPU%   MEM%${font sans-serif:normal:size=10}
+${font Inconsolata:pixelsize=14}
+${color orange}processes ${hr 2}${color}
+
+${color lightblue}NAME $alignr PID    CPU%    MEM%${color}
 ${top name 1} $alignr ${top pid 1} ${top cpu 1}% ${top mem 1}%
 ${top name 2} $alignr ${top pid 2} ${top cpu 2}% ${top mem 2}%
 ${top name 3} $alignr ${top pid 3} ${top cpu 3}% ${top mem 3}%
@@ -73,5 +67,8 @@ ${top name 7} $alignr ${top pid 7} ${top cpu 7}% ${top mem 7}%
 ${top name 8} $alignr ${top pid 8} ${top cpu 8}% ${top mem 8}%
 ${top name 9} $alignr ${top pid 9} ${top cpu 9}% ${top mem 9}%
 
+${color orange}fortune ${hr 2}${color}
+
+${execi 300 fortune | fold -w50}
 ${font}
 ]]
